ad6d2f83abdc84c357a64d10a05c7aa27fd11f34
Brycen Williams <brycenjwill@gmail.com>
2023-04-27 09:24:54 -0600

    Completed Wk2 Task Js

--- week2/scripts/task2.js
+++ week2/scripts/task2.js
@@ -3,54 +3,57 @@
 /* VARIABLES */
 
 // Step 1: declare and instantiate a variable to hold your name
+    let MyName = "Brycen";
 
 
 // Step 2: place the value of the name variable into the HTML file (hint: document.querySelector())
 
+    document.querySelector("#name").innerHTML = MyName;
+
 
 // Step 3: declare and instantiate a variable to hold the current year
-
+    let CurYear = new Date().getFullYear();
 
 // Step 4: place the value of the current year variable into the HTML file
-
+    document.querySelector("#year").innerHTML = CurYear;
 
 // Step 5: declare and instantiate a variable to hold the name of your picture
-
+    let MePhoto = 'images/IMG_2960.jpg'
 
 // Step 6: copy your image into the "images" folder
 
 // Step 7: place the value of the picture variable into the HTML file (hint: document.querySelector().setAttribute())
-
+    document.querySelector('img').setAttribute('src', MePhoto);
 
 
 
 /* ARRAYS */
 
 // Step 1: declare and instantiate an array variable to hold your favorite foods
-
+    let foods = [' Sushi', ' Salads', ' Italian Soda', ' Bananas'];
 
 // Step 2: place the values of the favorite foods variable into the HTML file
-
+    document.querySelector("#food").innerHTML = foods;
 
 // Step 3: declare and instantiate a variable to hold another favorite food
-
+    let addFood = ' Gnocci'
 
 // Step 4: add the variable holding another favorite food to the favorite food array
-
+    foods.push(addFood);
 
 // Step 5: repeat Step 2
-
+    document.querySelector("#food").innerHTML = foods;
 
 // Step 6: remove the first element in the favorite foods array
-
+    foods.shift();
 
 // Step 7: repeat Step 2
-
+    document.querySelector("#food").innerHTML = foods;  
 
 // Step 8: remove the last element in the favorite foods array
-
+    foods.pop();
 
 // Step 7: repeat Step 2
-
+    document.querySelector("#food").innerHTML = foods;  
 
 
